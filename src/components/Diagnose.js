c41dfb8832994d3e47833c66b847ee49cdc2066d
marekprog <marek.pierzchala.programista@gmail.com>
2020-07-26 20:15:42 +0200

    added medicine modal and 5 filters completed

--- src/components/Diagnose.js
+++ src/components/Diagnose.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import Dane from "../dane.json";
+import MedicineModal from "../components/MedicineModal";
 
 const Diagnose = (props) => {
   const dane = Dane;
@@ -7,6 +8,7 @@ const Diagnose = (props) => {
   const [params, setParams] = useState({
     rozpozn_hist: undefined,
     mutacja_aktywujaca_EFGR: undefined,
+    zaawansowanie: undefined,
   });
   useEffect(() => {
     if (props.newParamList) {
@@ -21,6 +23,7 @@ const Diagnose = (props) => {
     const paramsToScore = props.newParamList;
 
     const filterRozpoznHist = (item) => {
+      console.log(item);
       const finder = item.rozpozn_hist.find(
         (rak) => rak === paramsToScore.rozpozn_hist
       );
@@ -32,6 +35,26 @@ const Diagnose = (props) => {
         item.mutacja_aktywujaca_EFGR === paramsToScore.mutacja_aktywujaca_EFGR
       );
     };
+
+    const filterZaawansowanie = (item) => {
+      if (item.zaawansowanie) {
+        const finder = item.zaawansowanie.find(
+          (rak) => rak === paramsToScore.zaawansowanie
+        );
+        return !!finder;
+      }
+    };
+
+    const filterZmianyMierzalne = (item) => {
+      if (item.zmiany_mierzalne)
+        return item.zmiany_mierzalne === paramsToScore.zmiany_mierzalne;
+    };
+
+    const filterBrakPrzerzutow = (item) => {
+      if (item.brak_przerzutow_OUN)
+        return item.brak_przerzutow_OUN === paramsToScore.brak_przerzutow_OUN;
+    };
+
     //first filter
     let dataFilterRozpoznHist = [];
     !!props.newParamList.rozpozn_hist
@@ -42,7 +65,27 @@ const Diagnose = (props) => {
     !!props.newParamList.mutacja_aktywujaca_EFGR
       ? (dataFilterEFGR = dataFilterRozpoznHist.filter(filterEGFR))
       : (dataFilterEFGR = dataFilterRozpoznHist);
-    setDane(dataFilterEFGR);
+
+    //third filter
+    let dataFilterZaawansowanie = [];
+    !!props.newParamList.zaawansowanie
+      ? (dataFilterZaawansowanie = dataFilterEFGR.filter(filterZaawansowanie))
+      : (dataFilterZaawansowanie = dataFilterEFGR);
+    //fourth filter
+    let dataZmianyMierzalne = [];
+    !!props.newParamList.zmiany_mierzalne
+      ? (dataZmianyMierzalne = dataFilterZaawansowanie.filter(
+          filterZmianyMierzalne
+        ))
+      : (dataZmianyMierzalne = dataFilterZaawansowanie);
+
+    //fifth filter
+    let dataBrakPrzerzutow = [];
+    !!props.newParamList.brak_przerzutow_OUN
+      ? (dataBrakPrzerzutow = dataZmianyMierzalne.filter(filterBrakPrzerzutow))
+      : (dataBrakPrzerzutow = dataZmianyMierzalne);
+
+    setDane(dataBrakPrzerzutow);
   };
 
   return (
@@ -50,7 +93,18 @@ const Diagnose = (props) => {
       <h1>Diagnoza</h1>
       <ol>
         {daneF.map((lek) => {
-          return <li>{lek.name}</li>;
+          const keysMeds = Object.keys(lek).map((e) => e);
+          const valsMeds = Object.values(lek).map((e) => e);
+
+          return (
+            <li>
+              <MedicineModal
+                buttonLabel={lek.name}
+                properties={keysMeds}
+                values={valsMeds}
+              />
+            </li>
+          );
         })}
       </ol>
     </div>
---
+++ src/components/MedicineModal.js
@@ -0,0 +1,36 @@
+import React, { useState } from "react";
+import { Button, Modal, ModalHeader, ModalBody, ModalFooter } from "reactstrap";
+
+const ModalExample = (props) => {
+  const { buttonLabel, className, properties, values } = props;
+
+  const [modal, setModal] = useState(false);
+
+  const toggle = () => setModal(!modal);
+
+  return (
+    <div>
+      <Button color="danger" onClick={toggle}>
+        {buttonLabel}
+      </Button>
+      <Modal isOpen={modal} toggle={toggle} className={className}>
+        <ModalHeader toggle={toggle}>{buttonLabel}</ModalHeader>
+        <ModalBody>
+          <ol>
+            {properties.map((property) => {
+              return <li>{property}</li>;
+            })}
+          </ol>
+          <ol>
+            {values.map((value) => {
+              return <li>{value}</li>;
+            })}
+          </ol>
+        </ModalBody>
+        <ModalFooter></ModalFooter>
+      </Modal>
+    </div>
+  );
+};
+
+export default ModalExample;
